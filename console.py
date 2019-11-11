2d8f945bad281a9f07fd1ad08e0db291556fb862
German Franco F <933@holbertonschool.com>
2019-11-11 15:19:36 +0000

    [Add] add method create

--- console.py
+++ console.py
@@ -6,7 +6,7 @@ Console Module to handle Objects
 
 
 from cmd import Cmd
-
+from models.base_model import BaseModel
 class HBNBCommand(Cmd):
     '''HBNBCommand Console class'''
     
@@ -17,6 +17,17 @@ class HBNBCommand(Cmd):
         return True
 
     do_EOF = do_quit
+
+    def do_create(self, inp):
+        '''Creates a new instance of BaseModel'''
+        if inp == '':
+            print('** class name missing **')
+            return
+        if inp != 'BaseModel':
+            print("** class doesn't exist **")
+            return
+        base_model = BaseModel()
+        print(base_model.id)
 
 
 if __name__ == '__main__':
